2ba2c8963e16246d7d1fa28e545d2039764ffd84
ReenaH <Reena_Hunjan@yahoo.com>
2022-10-07 20:29:49 -0400

    updating

--- server/models/Spot.js
+++ server/models/Spot.js
@@ -7,25 +7,27 @@ const spotSchema = new Schema({
 
   city: {
     type: String,
-    
+    required: true,
+  },
   country: {
     type: String,
-   
+    required: true,
+  },
   climate: {
     type: String,
-   
+    required: true,
   },
   description: {
     type: String,
- 
+    required: true,
   },
   category: {
     type: String,
- 
+    required: true,
   },
   image: {
     type: String,
-     
+    required: true,
   },
   treat: {
     type: String,
